44cf7bf81473c1b365051980d0cc66283c37300e
Meenal Meena <34161491+meenal1303@users.noreply.github.com>
2019-05-06 16:25:22 +0530

    Play Pause feature added
    Floating text needs to be done

---
+++ PauseAndResume.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseAndResume : MonoBehaviour
+{
+	public bool pause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onPause(){
+    	pause=!pause;
+    	if(!pause){
+    		Time.timeScale=1;
+    	}else if (pause){
+    		Time.timeScale = 0;
+    	}
+    }
+}
